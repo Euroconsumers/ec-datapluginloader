83737528c32f6aebced1d86827b6164a43806d3b
Ken Van Gilbergen <mail@ken.be>
2017-08-20 15:21:01 +0200

    [WIP] Adding dependency loader.

--- src/ec-datapluginloader.js
+++ src/ec-datapluginloader.js
@@ -17,20 +17,39 @@
         console.log(sortedDataPlugins);
 
         //load non-existing
+        var appendScriptPromises = [];
         for (const prop in sortedDataPlugins.nonexistent) {
             console.log(prop);
             //console.log(sortedDataPlugins.nonexistent[prop][0]);
 
+            //resolve urls
             let widgetFullname = 'ec-' + prop;
             let rootUrl = 'https://cdn.euroconsumers.org/vendor/euroconsumers/';
             let widgetUrl = rootUrl + widgetFullname + '/0.0.1/';
+            let dependenciesUrl = widgetUrl + 'dependencies.json';
+            
+            //load dependencies
+            getUrl(dependenciesUrl).then(function (dependencies) {
+                console.log(dependencies);
+                loadDependencies(dependencies).then(() => {
+                    console.log('Dependency loaded.');
+                })
+            });
 
-            //TODO: load dependencies
-
-            loadScript(widgetUrl + widgetFullname + '.min.js');
+            appendScriptPromises.push(appendScript(widgetUrl + widgetFullname + '.min.js'));
         }
 
-        //set all settings
+        Promise.all(appendScriptPromises).then(function (result) {
+            console.log('Script loading finished.');        
+            console.log(result);
+
+            //TODO: set all settings
+
+        }).catch((err) => {
+            console.error('Dynamic script loading failed.');
+            console.log(err);
+        });
+
     }        
 
     const filterDataPlugins = (dataPlugins) => {
@@ -69,40 +88,82 @@
         return result;
     }
 
-    const loadScript = (url) => {
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = url;
-        script.async = false;
-        document.body.appendChild(script);
-        script.onload = function () {
-          console.log(url + ' loaded.');
-        };
-        // script.onerror = function (e) {
-        //   console.warn(e);
-        // };
+    const loadDependencies = (dependencies) => {
+        return new Promise(function (resolve, reject) {
+            //load css
+            let cssPromises = [];
+            for (const dependencyName in dependencies.css) {
+                //console.log(dependencyName);
+                //console.log(dependencies.css[dependencyName]);
+                cssPromises.push(appendStyle(dependencies.css[dependencyName]));
+            }
+            //load js
+            let jsPromises = [];
+            for (const dependencyName in dependencies.js) {
+                //console.log(dependencyName);
+                //console.log(dependencies.js[dependencyName]);
+                jsPromises.push(appendScript(dependencies.js[dependencyName]));
+            }
+            Promise.all([...cssPromises, ...jsPromises]).then(resolve).catch((err) => { reject(err)});
+        });
     }
 
-    const loadDataPluginAndDependencies = (url, pluginNameCamelCase, ns, ext) => {
+    const getUrl = (url) => {
+        return new Promise(function (resolve, reject) {
+          var xhr = new XMLHttpRequest();
+          xhr.addEventListener('load', function () {
+            if (xhr.status >= 200 && xhr.status < 400) {
+              resolve(JSON.parse(xhr.responseText));
+            } else {
+              reject(xhr.status);
+            }
+          });
+          xhr.open('get', url, true);
+          xhr.send();
+        });
+    };
+
+    const appendScript = (url) => {
         return new Promise((resolve, reject) => {
-          getDependenciesForNonExistentPlugins(`${url}dependencies.json`).then(data => {
-            loadDependencies(data);
-          }).then(() => {
-            const script = document.createElement('script');
+            var script = document.createElement('script');
             script.type = 'text/javascript';
-            script.src = `${url}${ns}-${pluginNameCamelCase}${ext}`;
+            script.src = url;
             script.async = false;
             document.body.appendChild(script);
-            script.onload = () => {
-              //console.log('script success');
-              resolve();
-            };
-            script.onerror = () => {
-              console.debug('Script Error');
-              reject();
-            };
-          });
-        });
+            script.onload = function () {
+                console.log(url + ' loaded.');
+                resolve();
+            }
+            script.onerror = function (err) {
+                //console.warn(e);
+                reject(err);
+            }
+        })
+    }
+
+    /**
+     * Dynamically load stylesheet
+     * Also, see {@link https://stackoverflow.com/questions/574944/how-to-load-up-css-files-using-javascript}
+     */
+    const appendStyle = (url) => {
+        return new Promise((resolve, reject) => {
+            var head  = document.getElementsByTagName('head')[0];
+            var link  = document.createElement('link');
+            //link.id   = cssId; // you could encode the url for this
+            link.rel  = 'stylesheet';
+            link.type = 'text/css';
+            link.href = url;
+            link.media = 'all';
+            head.appendChild(link);
+            link.onload = function () {
+                console.log(url + ' loaded.');
+                resolve();
+            }
+            link.onerror = function (err) {
+                //console.warn(err);
+                reject(err);
+            }
+        })
     }
 
 })(jQuery)
