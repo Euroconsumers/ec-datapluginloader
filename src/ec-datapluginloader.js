b86659b38d17567e2ebadcc156b54eb2942746aa
RaphDesaegher <raphael.desaegher@gmail.com>
2017-12-11 15:45:05 +0100

    Improve the loader
    Several improvements :
    * Add comments
    * Generalize the use of the cdn url (provided through options)
    * Add support for the new structure in the dependencies.json (flat array with css & js files mixed inside).
    * Add some utility functions in order to prepare the "cleaning" of the scripts (getFileExtension, getVersionNumber, getFileName).

--- src/ec-datapluginloader.js
+++ src/ec-datapluginloader.js
@@ -1,14 +1,19 @@
 (function() {
     'use strict';
+    /**
+     * 
+     * @param {*} options 
+     */
     window.initialiseWidgets = async (options) => {
         let scripts = [[], []], styles = [], promises = [];
+
+        options.cdnUrl = options.cdnUrl || 'https://cdn.euroconsumers.org';
 
         const widgets = getWidgets(options);
 
         //Get all the dependencies and put them together
         for(let widget in widgets){
-
-            const dependencies =  await getDependencies(widgets[widget].urls.dependencies);
+            const dependencies =  await getDependencies(widgets[widget].urls.dependencies,options);
 
             scripts[1].push(widgets[widget].urls.script);
 
@@ -22,6 +27,9 @@
                 return styles.indexOf(item) < 0;
             }));
         }
+
+        //Inspect the dependencies and clean them
+        cleanScripts(scripts);
 
         // Load all scripts and styles
         promises.push(loadScripts(scripts));
@@ -47,14 +55,13 @@
     } 
 
     /**
-     * Get the list of data-plugin used on the page.
-     * 
+     * Get the list of widget used on the page.
      * @param {object} {cdnUrl} the cdn Url passed in the options of the initialiseWidgets function
-     * @returns {Array} an array containing the list of all widgets used on the page.
+     * @returns {array} an array containing the list of all widgets used on the page.
      */
     const getWidgets = ({cdnUrl}) => {
         /**
-         * REMAINING TODO'S :
+         * REMAINING 'S :
          * - Handle version madness !
          */
         let dataPlugins = document.body.querySelectorAll('[data-plugin],[data-widget]'), widgets = {};
@@ -100,7 +107,6 @@
                 widgets[name].elements = [item];
 
                 // build the differents urls from the widget
-                cdnUrl = cdnUrl || 'https://cdn.euroconsumers.org';
                 let rootUrl =`${cdnUrl}/vendor/euroconsumers/ec-${name}/${version}/`;
                 widgets[name].urls ={
                     script : `${rootUrl}ec-${name}.min.js`,
@@ -118,11 +124,9 @@
      * @param {string} dependenciesUrl - url to the dependency JSON file. 
      * @return {object} An object containing both Css & JS dependencies (as arrays) of a specific widget.
      */
-    const getDependencies = async (dependenciesUrl) => {
+    const getDependencies = async (dependenciesUrl,{cdnUrl}) => {
         /**
-         * REMAINING TODO'S
-         * - Remove cdn hostname from all the urls and replace it with the provided (or default one)
-         * - Test new structure.
+         * REMAINING TODO's
          * - Check version
          */
         let dependencies = {
@@ -131,35 +135,91 @@
         };
 
         const response = await fetch(dependenciesUrl);
-        if(response.ok){ //TODO Check if 'ok' is reliable
+        if(response.ok){
             const json = await response.json();
+
+            //New structure example. this can be used as test.
+            // const json = [
+            //     "/vendor/zxcvbn/4.4.2/dist/zxcvbn.min.js",
+            //     "/vendor/euroconsumers/ec-loadingbar/0.0.1/ec-loadingbar.min.css",
+            //      "cdn.google.com/script/1.0.0/script.js"
+            // ];
+
+            /********************************************************************************************************************************
+            * This code handle the new structure of the dependecies.json files. This structure is currently not implemented in any widget.  *
+            * The new structure looks like this the example in the previous comment                                                         *
+            *********************************************************************************************************************************/
+            if(Array.isArray(json)){
+                for(let dependency of json){
+
+                    //Check if the dependency contains a hostname & remove it if it's the case. 
+                    let parts = dependency.replace('//','X').split('/');
+                    if(parts[0].match(/^(?:https?:\/\/)?.+\.(?:.{2,3})/g)){
+                        parts.splice(0,1);
+                        dependency = `/${parts.join('/')}`;   
+                    }
+
+                    //the correct cdn hostname is here applied to all the dependencies.
+                    dependencies[getFileExtension(dependency)].push(`${cdnUrl}${dependency}`);
+                }
+                return dependencies;
+            }
+            /********************************************************************************
+            * This code only exist to support the old structure.                            *
+            * Once all the widgets are migrated to the new structure it can be removed.     *
+            * Replace it with an error message                                              *
+            *********************************************************************************/
             for(let type in json){
                 if(json.hasOwnProperty(type))
                 {
-                    if(Array.isArray(json[type])){
-                        dependencies[type] = json[type];
-                    }
-                    else {
-                        /********************************************************************************
-                        * This code only exist to support the old structure.                            *
-                        * Once all the widgets are migrated to the new structure it can be removed.     *
-                        * Replace it with an error message                                              *
-                        *********************************************************************************/
-                        for (let dependency in json[type]){
+                    for (let dependency in json[type]){
 
-                            if(typeof json[type][dependency] !== 'string')
-                            {
-                                console.error(`Formatting issue in dependencies in ${dependenciesUrl} : \n ${dependency} is not in a valid format for ${type}. it's a(n) ${typeof json[type][dependency]} but it should be a string`);
-                                continue;
-                            }
-
-                            dependencies[type].push(json[type][dependency]);
+                        if(typeof json[type][dependency] !== 'string')
+                        {
+                            console.error(`Formatting issue in dependencies in ${dependenciesUrl} : \n ${dependency} is not in a valid format for ${type}. it's a(n) ${typeof json[type][dependency]} but it should be a string`);
+                            continue;
                         }
+
+                        //Check if the dependency contains a hostname & remove it if it's the case. 
+                        let parts = json[type][dependency].replace('//','X').split('/');
+                        if(parts[0].match(/^(?:https?:\/\/)?.+\.(?:.{2,3})/g)){
+                            parts.splice(0,1);
+                            json[type][dependency] = `/${parts.join('/')}`;   
+                        }
+
+                        dependencies[type].push(`${cdnUrl}${json[type][dependency]}`);
                     }
                 }
             }
+        } else {
+            console.error(`Unable to get ${dependenciesUrl}. No dependencies will be loaded for this widget.`)
         }
         return dependencies;        
+    }
+
+    /**
+     * Check all the scripts. Remove the duplicates. Show warning if different versions are requested and remove the oldest.
+     * 
+     * @param {object} scripts 
+     */
+    const cleanScripts = (scripts) => {
+        /**
+         * TODO's :
+         *  Ensure that the min version is used. 
+         *  Ensure that the correct cdnUrl is used. 
+         *  Remove library duplicated with different versions.
+         *  Check that the script is not loaded yet. 
+         */
+        for(let group of scripts){
+            var groupIndex = scripts.indexOf(group);
+            for(let script of group){
+                // console.log(script);
+                // console.log(getFileExtension(script));
+                // console.log(getVersionNumber(script));
+                // console.log(getFileName(script))
+            }
+        }
+        
     }
 
     /**
@@ -233,4 +293,44 @@
             }
         })
     }
+
+    /**
+     * return the extension of the file in the given path.
+     * @param {string} path - file path
+     */
+    const getFileExtension = (path) =>{
+        let index = path.lastIndexOf('.');
+        if(index === -1 || index === path.length - 1){
+            console.error(`"${path}" is not a correct url.`);
+            return undefined;
+        }
+        return path.slice(index+1);
+    }
+
+    /**
+     * return the version of the file in the given path. This version number is retrieved by searching 
+     * @param {string} path - file path
+     */
+    const getVersionNumber = (path) => {
+        let versionEnd = path.lastIndexOf('/'),
+        versionStart = path.lastIndexOf('/',versionEnd - 1);
+        if(versionStart === -1 || versionEnd === path.length){
+            console.error(`"${path}" does not contain a version number.`);
+            return undefined;
+        }
+        return path.substring(versionStart+1,versionEnd)
+    }
+
+    const getFileName = (path) => {
+    //TODO : force min version to all filename. NEED TO BE CHECKED WITH KVG
+        let begin = path.lastIndexOf('/') + 1,
+            end = path.lastIndexOf('.');
+        if(begin >= end){
+            console.error(`"${path}" does not contain a version number.`);
+            return undefined;
+        }
+
+        return path.substring(begin,end);
+    }
+
 })()
