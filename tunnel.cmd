82d39d75dbb35ef570a10433959e354cbb09e0de
RaphDesaegher <raphael.desaegher@gmail.com>
2018-02-13 09:44:45 +0100

    Add tunnel executable
    Add everything to run a tunnel on a specific machine based on environment variable

---
+++ tunnel.cmd
@@ -0,0 +1,10 @@
+@echo off
+if "%SAUCE_USERNAME%"=="sso-euroconsumers-RDesaegher" GOTO :username-missing
+if "%SAUCE_ACCESS_KEY%"=="" GOTO :access-key-missing
+bin\sauceconnect  -u %SAUCE_USERNAME% -k %SAUCE_ACCESS_KEY% --proxy-tunnel  --no-autodetect -i "%SAUCE_USERNAME%'s Tunnel"
+GOTO :eof
+:username-missing
+    echo [91m ERROR : SAUCE_USERNAME is not defined. Please read the documentation to find out how to set it. [0m
+    GOTO :eof
+:access-key-missing
+    echo [91m ERROR : SAUCE_ACCESS_KEY is not defined. Please read the documentation to find out how to set it.[0m
